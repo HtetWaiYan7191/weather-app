2af4af03b3896c195cd63f4c318266447a36793d
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-20 21:06:43 +0630

    Add Error component for wrong url

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h2 className='text-4xl font-semibold'>404 NOT FOUND</h2>
+    </div>
+  )
+}
+
+export default ErrorPage
