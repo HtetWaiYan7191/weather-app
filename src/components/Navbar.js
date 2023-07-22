16d6fd2328596db50ccf1f01390058af1e012971
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-22 17:03:00 +0630

    Add mobile responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,14 +13,15 @@ const Navbar = () => {
     setInputSearch('');
   };
   return (
-    <nav className="flex justify-between px-10 py-5 items-center border-b-2 bg-sky-600">
+    <nav className=" flex flex-row-reverse md:flex md:flex-row justify-between px-10 py-5 items-center border-b-2 bg-sky-600">
       <div className=" logo-container">
         <TiWeatherPartlySunny className="text-7xl text-white" />
       </div>
       <form onSubmit={handleSubmit}>
-        <input placeholder="Search City" value={inputSearch} onChange={(e) => setInputSearch(e.target.value)} className=" outline-none border-b-2 text-white border-b-white bg-inherit placeholder:text-white" />
+        <input placeholder="Search City" value={inputSearch} onChange={(e) => setInputSearch(e.target.value)} className=" outline-none border-b-2 text-white border-b-white bg-inherit placeholder:text-white mb-3 md:mb-0" />
+        <button type="submit" onClick={handleSubmit} className=" md:hidden block hover:bg-sky-700 text-white bg-sky-800 rounded-md py-1 px-3">Search</button>
       </form>
-      <ul className="flex">
+      <ul className="md:flex hidden">
         <li className="me-3"><NavLink to="/">Home</NavLink></li>
         <li className="me-3"><Link to="/" target="blank">City API</Link></li>
         <li className="me-3"><Link to="/" target="blank">Weather API</Link></li>
