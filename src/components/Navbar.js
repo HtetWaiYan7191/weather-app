494b72aba3c9d4c4a51499bb9794f4d185cd25cd
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-31 10:21:34 +0630

    Fix eslint errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,12 +21,17 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`flex md:flex ${detailPage || searchPage ? 'flex-row-reverse' : 'flex-row'} md:flex-row justify-between px-2 md:px-10 py-5 items-center border-b-2 bg-sky-600`}>
+    <nav className={`flex md:flex ${detailPage || searchPage ? '' : 'flex-row'} md:flex-row justify-between px-2 md:px-10 py-5 items-center border-b-2 bg-sky-600`}>
       <Link to="/" className={`logo-container ${detailPage || searchPage ? 'hidden' : 'flex'}`}>
         <TiWeatherPartlySunny className="text-7xl text-white" />
       </Link>
       {detailPage || searchPage ? (
-        <IoMdArrowRoundBack onClick={handleClick} className="text-4xl font-semibold text-white ms-10 border cursor-pointer border-white" />
+        <>
+          <IoMdArrowRoundBack onClick={handleClick} className="text-4xl font-semibold text-white ms-10 border cursor-pointer border-white" />
+          <div className="menu-container md:hidden block">
+            <AiOutlineMenu className="text-white text-6xl pe-4" onClick={() => { setToggle(!toggle); }} />
+          </div>
+        </>
       ) : (
         <div className="menu-container md:hidden block">
           <AiOutlineMenu className="text-white text-6xl pe-4" onClick={() => { setToggle(!toggle); }} />
@@ -35,14 +40,30 @@ const Navbar = () => {
 
       <ul className="md:flex  hidden">
         <li className="me-3"><NavLink to="/">Home</NavLink></li>
-        <li className="me-3"><Link to="/" target="blank">City API</Link></li>
-        <li className="me-3"><Link to="/" target="blank">Weather API</Link></li>
+        <li className="me-3">
+          <a href="https://openweathermap.org/api" target="_blank" rel="noopener noreferrer">
+            Weather API
+          </a>
+        </li>
+        <li className="me-3">
+          <a href="https://openweathermap.org/api/geocoding-api" target="_blank" rel="noopener noreferrer">
+            City API
+          </a>
+        </li>
       </ul>
       <div className={`flex mobile-list-container absolute slide-in ${toggle ? 'left-0 top-0' : 'left-[-100%] top-0'} bg-sky-300 z-20 `}>
         <ul className="m-10">
-          <li className="my-5 text-4xl"><NavLink to="/">Home</NavLink></li>
-          <li className="my-5 text-4xl"><Link to="/" target="blank">City API</Link></li>
-          <li className="my-5 text-4xl"><Link to="/" target="blank">Weather API</Link></li>
+          <li className="my-5 text-4xl"><NavLink to="/" onClick={() => setToggle(!toggle)}>Home</NavLink></li>
+          <li className="my-5 text-4xl">
+            <a href="https://openweathermap.org/api" target="_blank" rel="noopener noreferrer">
+              Weather API
+            </a>
+          </li>
+          <li className="my-5 text-4xl">
+            <a href="https://openweathermap.org/api/geocoding-api" target="_blank" rel="noopener noreferrer">
+              City API
+            </a>
+          </li>
         </ul>
         <RxCross1 className="text-4xl text-white my-7" onClick={() => setToggle(!toggle)} />
       </div>
